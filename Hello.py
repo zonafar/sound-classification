a76d96ec5141fd75d7f99fd2ad45a03e7f2f9340
zonafar <95772160+zonafar@users.noreply.github.com>
2024-03-19 12:54:20 +0000

    new update

--- Hello.py
+++ Hello.py
@@ -1,50 +1,101 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import streamlit as st
+import os
+import json
 from streamlit.logger import get_logger
+from gradio_client import Client
+import math
+from dotenv import load_dotenv
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import matplotlib.pyplot as plt
+import numpy as np
+import librosa.display
 
 LOGGER = get_logger(__name__)
+
+# Client de l'API 
+client = Client("https://abdoulgafar-woodsound.hf.space/")
+
+# stocker le fichier 
+def save_file(uploaded_file):
+    file_path = os.path.join("uploads", uploaded_file.name)
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    return file_path
+
+# Fonction d'envoie de mail
+def send_email(receiver_email, subject, message):
+    load_dotenv() # charger les secrets de .env
+    sender_email = os.getenv("GMAIL_ADDRESS")  
+    sender_password = os.getenv("GMAIL_PASSWORD")
+
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message, 'plain'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        text = msg.as_string()
+        server.sendmail(sender_email, receiver_email, text)
+        st.success("E-mail envoyé avec succès!")
+    except Exception as e:
+        st.error(f"Erreur lors de l'envoi de l'e-mail: {e}")
+    finally:
+        server.quit()
 
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="Application",
         page_icon="👋",
     )
+    st.title("👋 Détection de son de coupe de bois")
 
-    st.write("# Welcome to Streamlit! 👋")
+    uploaded_file = st.file_uploader("Sélectionnez ou enregistrer un fichier audio (wav, mp3, etc.)")
 
-    st.sidebar.success("Select a demo above.")
+    # Charger le fichier, consommer l'api, et recupérer le retour
+    if uploaded_file is not None:
+        st.subheader("Extrait audio")
+        st.audio(uploaded_file)
+        result = client.predict(save_file(uploaded_file), api_name="/predict")
+        # st.write("Résultat de la prédiction :", result)       
+        with open(result[1], 'r') as f:
+            data = json.load(f)
+        predictions = data["confidences"]
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+        # predictions et labels
+        st.subheader("Prédictions")
+        for pred in predictions:
+            col1, col2, col3 = st.columns(3)
+            col1.write(pred['label'])
+            col2.progress(pred['confidence'])
+            col3.info(math.trunc(pred['confidence'] * 100) / 100)
+
+        st.subheader("Spectrogramme")
+        st.image(result[0]) 
+
+        # envoie de notification mail
+        first_prediction = predictions[0]
+        if (first_prediction["label"] == "scie électrique" ) and (first_prediction["confidence"] > 0.7) :
+            st.success("Envoie d'une notification en cours ...")
+            # charger secret
+            load_dotenv()
+            receiver_email = os.getenv("GMAIL_RECEIVER")
+            subject = "Détection de son de coupe de bois"
+            message = f"\n {first_prediction['label']} \n {first_prediction['confidence']}"
+
+            # send_email(receiver_email, subject, message)
+        
+
+      
+        
 
 
 if __name__ == "__main__":
